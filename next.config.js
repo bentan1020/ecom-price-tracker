ac804784faba30c5ffe74ffad4921bf4a3f26fd6
ben <bentan1020@gmail.com>
2023-10-07 17:25:42 -0400

    Product details + similar product functionality

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false, module: false };
-
-    return config;
-  },
   experimental: {
     serverActions: true,
-    serverComponentsExternalPackages: ['mongoose']
+    serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ['m.media-amazon.com']
-  }
+    domains: ["m.media-amazon.com"],
+  },
 };
 
 module.exports = nextConfig;
