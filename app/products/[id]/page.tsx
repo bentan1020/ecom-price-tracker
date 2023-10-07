99efe1a50ed72438f0b216c297c72fc7fcfa246a
ben <bentan1020@gmail.com>
2023-10-07 19:28:42 -0400

    Implement cron job

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -3,6 +3,7 @@ import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
 import { getProductById, getSimilarProducts } from "@/lib/actions";
 import { formatNumber } from "@/lib/utils";
+import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -12,7 +13,7 @@ type Props = {
 }
 
 const ProductDetails = async ({ params: { id } }: Props) => {
-    const product = await getProductById(id);
+    const product: Product = await getProductById(id);
 
     if (!product) redirect('/')
 
